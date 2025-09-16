2a57be87855090a1f88fac8660028a1492646f5d
rahman <rahmannurhayadi@gmail.com>
2025-09-16 11:01:30 +0700

    add base routes

--- pos-be/index.js
+++ pos-be/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
+import router from './src/router/router.js'
 const port = 3000
 const app = express()
+
+app.use('/api',router)
 
 app.listen(port,()=>{
     console.warn(`server running on port ${port}`);
