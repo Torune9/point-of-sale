25da44d1fe25f09dacaae53d76ce6bf084183a35
rahman <rahmannurhayadi@gmail.com>
2025-09-18 16:32:49 +0700

    feat(router) : add role router

---
+++ pos-be/src/router/routes/roleRoutes.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import { validateData } from "../../middleware/validationMiddleware.js"
+import { Role } from "../../schemas/roleSchema.js"
+import { createRole } from "../../controller/roles/create.js"
+import { getRoles } from "../../controller/roles/get.js"
+import { updateRole } from "../../controller/roles/update.js"
+
+export const roleRouter = express.Router()
+
+roleRouter.get('/',getRoles)
+
+roleRouter.post('/',validateData(Role),createRole)
+
+roleRouter.patch('/:id',validateData(Role),updateRole)
