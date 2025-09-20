15ce8724af75de63573710eaf73cb70525c2ec59
rahman <rahmannurhayadi@gmail.com>
2025-09-20 09:49:03 +0700

    feat(router) : add worker router in router.ts

--- pos-be/src/router/router.ts
+++ pos-be/src/router/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 import { authRouter } from "./routes/authRoutes.js"
 import { roleRouter } from "./routes/roleRoutes.js"
+import { workerRouter } from "./routes/workerRoutes.js"
 const router = Router()
 
 router.use('/auth',authRouter)
-router.use('/role',roleRouter)
+router.use('/roles',roleRouter)
+router.use('/workers',workerRouter)
 
 export default router
