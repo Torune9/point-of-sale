0c81a6e13c36386f9301157a57f1f709a4469e1e
rahman <rahmannurhayadi@gmail.com>
2025-09-30 08:37:02 +0700

    feat(router) : add category router

--- pos-be/src/router/router.ts
+++ pos-be/src/router/router.ts
@@ -2,10 +2,12 @@ import { Router } from "express"
 import { authRouter } from "./routes/authRoutes.js"
 import { roleRouter } from "./routes/roleRoutes.js"
 import { workerRouter } from "./routes/workerRoutes.js"
+import { categoryRouter } from "./routes/categoryRoutes.js"
 const router = Router()
 
 router.use('/auth',authRouter)
 router.use('/roles',roleRouter)
 router.use('/workers',workerRouter)
+router.use('/category',categoryRouter)
 
 export default router
