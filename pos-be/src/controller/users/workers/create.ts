396af23fcea6b53e610c995adadd94b5793854aa
rahman <rahmannurhayadi@gmail.com>
2025-10-01 08:23:46 +0700

    fix(controller) : delete line code for checking email already exist,handle by next func

--- pos-be/src/controller/users/workers/create.ts
+++ pos-be/src/controller/users/workers/create.ts
@@ -1,23 +1,13 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 
 import prisma from "../../../utils/prisma.js";
 
 import bcrypt from 'bcrypt'
 import { Prisma } from "@prisma/client";
 
-export const createWorkers = async (req: Request, res: Response) => {
+export const createWorkers = async (req: Request, res: Response,next:NextFunction) => {
     try {
         const { username, email, password, roleId, businessId } = req.body;
-
-        const existing = await prisma.workers.findUnique({
-            where: { email },
-        });
-
-        if (existing) {
-            return res.status(409).json({
-                message: "email already taken, please change it",
-            });
-        }
 
         const hashPassword = await bcrypt.hash(password, 10);
 
@@ -38,10 +28,7 @@ export const createWorkers = async (req: Request, res: Response) => {
         });
     } catch (error) {
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            return res.status(500).json({
-                code: res.statusCode,
-                errors: error,
-            });
+            next(error)
         }
 
         return res.status(500).json({
