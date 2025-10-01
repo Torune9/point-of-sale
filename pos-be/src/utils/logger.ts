8866e05035ac2bce452663ed892d3cd15b01f777
rahman <rahmannurhayadi@gmail.com>
2025-10-01 08:21:33 +0700

    fix(logger) : set max size for file and set level for file transport

--- pos-be/src/utils/logger.ts
+++ pos-be/src/utils/logger.ts
@@ -1,8 +1,10 @@
 import winston from "winston";
 
+const MAX_SIZE = 5 * 1024 * 1024
+
 export const logger = winston.createLogger({
     level: process.env.LEVEL_LOGGER || 'debug',
-    handleExceptions : true,
+    handleExceptions: true,
     format: winston.format.combine(
         winston.format.colorize(),
         winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
@@ -16,7 +18,14 @@ export const logger = winston.createLogger({
     transports: [
         new winston.transports.Console({}),
         new winston.transports.File({
-            filename : 'logs/exception.log',
+            level: 'info',
+            filename: 'logs/exception.log',
+            maxsize: MAX_SIZE
+        }),
+        new winston.transports.File({
+            level: 'error',
+            filename: 'logs/errors.log',
+            maxsize: MAX_SIZE
         })
     ]
 })
