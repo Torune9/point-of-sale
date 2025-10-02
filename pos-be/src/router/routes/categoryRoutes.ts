071e355e00e816612ddc5582ee8624ac07f5e302
rahman <rahmannurhayadi@gmail.com>
2025-10-02 09:04:01 +0700

    refactor(router) : add spacing on category router

--- pos-be/src/router/routes/categoryRoutes.ts
+++ pos-be/src/router/routes/categoryRoutes.ts
@@ -1,5 +1,4 @@
 import e from "express";
-import { validateData } from "../../middleware/validationMiddleware.js";
 import { createCategory } from "../../controller/inventories/categories/create.js";
 import { getCategory, getCategoryById } from "../../controller/inventories/categories/get.js";
 import { updateCategory } from "../../controller/inventories/categories/update.js";
@@ -8,7 +7,11 @@ import { deleteCategory } from "../../controller/inventories/categories/delete.j
 export const categoryRouter = e.Router()
 
 categoryRouter.get('/', getCategory)
+
 categoryRouter.post('/', createCategory)
+
 categoryRouter.get('/:id', getCategoryById)
+
 categoryRouter.patch('/:id', updateCategory)
+
 categoryRouter.delete('/:id', deleteCategory)
