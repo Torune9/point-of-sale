e559c343583c8d132aa1d06de050056dcf0463d8
rahman <rahmannurhayadi@gmail.com>
2025-10-02 08:51:47 +0700

    fix(utils) : add log in prismaClient

--- pos-be/src/utils/prisma.ts
+++ pos-be/src/utils/prisma.ts
@@ -1,5 +1,38 @@
 import { PrismaClient } from "@prisma/client";
+import { logger } from "./logger.js";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: [
+        {
+            emit: 'event',
+            level: 'query',
+        },
+        {
+            emit: 'stdout',
+            level: 'error',
+        },
+        {
+            emit: 'stdout',
+            level: 'info',
+        },
+        {
+            emit: 'stdout',
+            level: 'warn',
+        },
+    ],
+});
+
+prisma.$on('error',(e)=>{
+    logger.error(e)
+})
+prisma.$on('info',(e)=>{
+    logger.info(e)
+})
+prisma.$on('warn',(e)=>{
+    logger.warn(e)
+})
+prisma.$on('query',(e)=>{
+    logger.info(e.query)
+})
 
 export default prisma;
