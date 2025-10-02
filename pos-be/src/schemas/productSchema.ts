137faa98188b3080fe45bd462c47c7c111a9a236
rahman <rahmannurhayadi@gmail.com>
2025-10-02 09:43:28 +0700

    feat(schema) : add product schema

---
+++ pos-be/src/schemas/productSchema.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const Product = z.object({
+    name : z.string().min(4),
+    price : z.coerce.number().positive(),
+    stock : z.coerce.number().positive(),
+    categoryId : z.string(),
+    businessId : z.string()
+})
+
+
+export const ProductUpdate = Product.partial({
+    categoryId :true
+})
