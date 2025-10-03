11605425eabeb017bbe1eb84f704bc3a395fbe26
rahman <rahmannurhayadi@gmail.com>
2025-10-03 17:04:19 +0700

    feat(controller) : add stockReceive func

---
+++ pos-be/src/controller/inventories/stock-movements/stockReceive.ts
@@ -0,0 +1,97 @@
+import type { Request, Response } from "express";
+import prisma from "../../../utils/prisma.js";
+
+export const stockMovement = async (req: Request, res: Response) => {
+    try {
+        const { quantity, type, productId, note } = req.body
+
+        const product = await prisma.product.findFirst({
+            where: {
+                id: productId
+            }
+        })
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found",
+                code: res.statusCode,
+            });
+        }
+
+        if (product.stock == 0 || quantity > product.stock) {
+            return res.status(400).json({
+                message: 'out of stock',
+                code: res.statusCode
+            })
+        }
+
+        if (type === 'OUT') {
+            const [stockMovement, product] = await prisma.$transaction([
+                prisma.stockMovement.create({
+                    data: {
+                        quantity,
+                        type,
+                        productId,
+                        note
+                    },
+                    omit: {
+                        updatedAt: true,
+                    },
+                }),
+                prisma.product.update({
+                    where: {
+                        id: productId as string
+                    },
+                    data: {
+                        stock: {
+                            decrement: quantity
+                        }
+                    },
+                })
+            ])
+
+            return res.json({
+                message: 'product has been out stock',
+                dataReceive: stockMovement,
+                dataUpdate: product
+            })
+        } else if (type === 'IN') {
+            const [stockMovement, product] = await prisma.$transaction([
+                prisma.stockMovement.create({
+                    data: {
+                        quantity,
+                        type,
+                        productId,
+                        note
+                    },
+                    omit: {
+                        updatedAt: true,
+                    },
+                }),
+                prisma.product.update({
+                    where: {
+                        id: productId as string
+                    },
+                    data: {
+                        stock: {
+                            increment: quantity
+                        }
+                    },
+                })
+            ])
+
+            return res.status(201).json({
+                message: 'product has been recap',
+                code: res.statusCode,
+                dataReceive: stockMovement,
+                dataUpdate: product,
+            })
+        }
+
+    } catch (error) {
+        return res.status(500).json({
+            message: 'error when create recap product on server',
+            code: res.statusCode,
+            errors: error
+        })
+    }
+}
