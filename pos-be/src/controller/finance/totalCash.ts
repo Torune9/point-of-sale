b15429d2c65635068c5dc53b6f7e1ee1bdae0b59
rahman <rahmannurhayadi@gmail.com>
2025-10-04 10:58:44 +0700

    feat(controller) : add get total cash func

---
+++ pos-be/src/controller/finance/totalCash.ts
@@ -0,0 +1,41 @@
+import type { Request, Response } from "express";
+import prisma from "../../utils/prisma.js";
+
+export const totalCash = async (req: Request, res: Response) => {
+    try {
+        const { businessId } = req.params
+        const totalCash = await prisma.cashflow.aggregate({
+            where: {
+                businessId: businessId as string,
+                type: "IN"
+            },
+            _sum: {
+                amount: true
+            }
+        })
+        const totalOutCash = await prisma.cashflow.aggregate({
+            where: {
+                businessId: businessId as string,
+                type: "OUT",
+            },
+            _sum: {
+                amount: true,
+            },
+        });
+
+        const totalInValue = totalCash._sum.amount || 0;
+        const totalOutValue = totalOutCash._sum.amount || 0;
+        return res.json({
+            totalIn: totalInValue,
+            totalOut: totalOutValue,
+            balance: totalInValue - totalOutValue,
+        });
+    } catch (error) {
+        return res.status(500).json(
+            {
+                message: "Internal server error",
+                error
+            }
+        );
+    }
+}
