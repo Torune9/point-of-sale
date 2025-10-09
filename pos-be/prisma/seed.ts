60ec39321e8aad55533ce5e08a038d876ad2f8b4
rahman <rahmannurhayadi@gmail.com>
2025-10-09 09:09:01 +0700

    call seeder function in main seed

--- pos-be/prisma/seed.ts
+++ pos-be/prisma/seed.ts
@@ -1,172 +1,35 @@
 import { PrismaClient } from '@prisma/client'
-import bcrypt from 'bcrypt'
-
+import { categorySeeder } from './seeders/categories.seed'
+import { roleSeeder } from './seeders/roles.seed'
+import { userSeeder } from './seeders/users.seed'
+import { productSeeder } from './seeders/product.seed'
+import { businessSeeder } from './seeders/bussines.seed'
+import { workerSeeder } from './seeders/workers.seed'
+import { stockMovementSeeder } from './seeders/stockMovement.seed'
+import { saleSeeder } from './seeders/sales.seed'
+import { cashflowSeeder } from './seeders/cashFlow.seed'
+import { logger } from '../src/utils/logger'
 const prisma = new PrismaClient()
 
 async function main() {
-  const saltRounds = 10
-  const hashedPassword = await bcrypt.hash('12345678', saltRounds)
-
-  // Roles
-  await prisma.roles.createMany({
-    data: [
-      { name: 'admin' },
-      { name: 'cashier' },
-      { name: 'manager' },
-      { name: 'warehouse' },
-      { name: 'owner' },
-    ],
-    skipDuplicates: true,
-  })
-
-  const adminRole = await prisma.roles.findFirst({ where: { name: 'admin' } })
-  const ownerRole = await prisma.roles.findFirst({ where: { name: 'owner' } })
-
-  if (!adminRole || !ownerRole) throw new Error('Roles not found!')
-
-  // Admin
-  await prisma.users.upsert({
-    where: { email: 'admin@mail.com' },
-    update: {},
-    create: {
-      email: 'admin@mail.com',
-      username: 'admin',
-      password: hashedPassword,
-      roleId: adminRole.id,
-    },
-  })
-
-  // Owner
-  const ownerUser = await prisma.users.upsert({
-    where: { email: 'owner@mail.com' },
-    update: {},
-    create: {
-      email: 'owner@mail.com',
-      username: 'owner',
-      password: hashedPassword,
-      roleId: ownerRole.id,
-    },
-  })
-
-  // Business
-  const business = await prisma.businesses.upsert({
-    where: { ownerId: ownerUser.id },
-    update: {},
-    create: {
-      name: 'Owner First Business',
-      ownerId: ownerUser.id,
-    },
-  })
-
-  // Categories
-  await prisma.category.createMany({
-    data: [
-      { name: 'Food' },
-      { name: 'Drink' },
-      { name: 'Office stationery' },
-      { name: 'Electronic' },
-      { name: 'Health' },
-    ],
-    skipDuplicates: true,
-  })
-
-  const categories = await prisma.category.findMany()
-  const getCategoryId = (name: string) =>
-    categories.find((c) => c.name === name)?.id || null
-
-  // Products
-  await prisma.product.createMany({
-    data: [
-      {
-        name: 'Instant Noodles',
-        sku: 'FOOD-001',
-        price: 2500,
-        stock: 100,
-        categoryId: getCategoryId('Food'),
-        businessId: business.id,
-      },
-      {
-        name: 'Biscuits',
-        sku: 'FOOD-002',
-        price: 5000,
-        stock: 50,
-        categoryId: getCategoryId('Food'),
-        businessId: business.id,
-      },
-      {
-        name: 'Mineral Water 600ml',
-        sku: 'DRINK-001',
-        price: 3000,
-        stock: 200,
-        categoryId: getCategoryId('Drink'),
-        businessId: business.id,
-      },
-      {
-        name: 'Cola Can 250ml',
-        sku: 'DRINK-002',
-        price: 7000,
-        stock: 80,
-        categoryId: getCategoryId('Drink'),
-        businessId: business.id,
-      },
-      {
-        name: 'Ballpoint Pen',
-        sku: 'OFFICE-001',
-        price: 4000,
-        stock: 120,
-        categoryId: getCategoryId('Office stationery'),
-        businessId: business.id,
-      },
-      {
-        name: 'Notebook A5',
-        sku: 'OFFICE-002',
-        price: 10000,
-        stock: 60,
-        categoryId: getCategoryId('Office stationery'),
-        businessId: business.id,
-      },
-      {
-        name: 'USB Cable',
-        sku: 'ELEC-001',
-        price: 15000,
-        stock: 40,
-        categoryId: getCategoryId('Electronic'),
-        businessId: business.id,
-      },
-      {
-        name: 'LED Bulb 10W',
-        sku: 'ELEC-002',
-        price: 20000,
-        stock: 70,
-        categoryId: getCategoryId('Electronic'),
-        businessId: business.id,
-      },
-      {
-        name: 'Vitamin C 500mg',
-        sku: 'HEALTH-001',
-        price: 25000,
-        stock: 90,
-        categoryId: getCategoryId('Health'),
-        businessId: business.id,
-      },
-      {
-        name: 'Pain Relief Ointment',
-        sku: 'HEALTH-002',
-        price: 18000,
-        stock: 40,
-        categoryId: getCategoryId('Health'),
-        businessId: business.id,
-      },
-    ],
-  })
+  const roles = await roleSeeder()
+  const users = await userSeeder(roles)
+  const businesses = await businessSeeder(users)
+  const categories = await categorySeeder(businesses)
+  const products = await productSeeder(categories,businesses)
+  const workers = await workerSeeder(businesses,roles)
+  const stockMovement = await stockMovementSeeder(businesses,products)
+  const sales = await saleSeeder(businesses[0],products)
+  const cashFlow = await cashflowSeeder(businesses,workers,sales)
+  
 }
 
 main()
   .then(() => {
-    console.log('Seeding done ✅')
+    logger.info('Seeding done')
   })
   .catch((e) => {
-    console.error(e)
+    logger.error(e)
     process.exit(1)
   })
   .finally(async () => {
