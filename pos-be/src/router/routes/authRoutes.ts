b948a4ec22027e1b2e9fda027f0a0d2ed9f23126
rahman <rahmannurhayadi@gmail.com>
2025-10-14 08:43:33 +0700

    feat(authRoutes) : add register & worker routes

--- pos-be/src/router/routes/authRoutes.ts
+++ pos-be/src/router/routes/authRoutes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { validateData } from "../../middleware/validationMiddleware.js";
-import { User } from "../../schemas/userSchema.js";
-import { login } from "../../controller/users/authController.js";
+import { User, UserLogin } from "../../schemas/userSchema.js";
+import { ownerLogin, workerLogin } from "../../controller/users/login.js";
+import { register } from "../../controller/users/register.js";
 
 export const authRouter = Router()
 
-authRouter.post('/',validateData(User),login)
+authRouter.post('/login', validateData(UserLogin), ownerLogin)
+
+authRouter.post('/login/worker', validateData(UserLogin), workerLogin)
+
+authRouter.post('/register', validateData(User), register)
