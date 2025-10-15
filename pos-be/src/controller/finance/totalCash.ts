85b2d8221aadea53a02ab821412ae1a8c59e0bde
rahman <rahmannurhayadi@gmail.com>
2025-10-15 11:55:01 +0700

    refactor(financeController) : set to promise when get cashflow using agg func

--- pos-be/src/controller/finance/totalCash.ts
+++ pos-be/src/controller/finance/totalCash.ts
@@ -4,27 +4,32 @@ import prisma from "../../utils/prisma.js";
 export const totalCash = async (req: Request, res: Response) => {
     try {
         const { businessId } = req.params
-        const totalCash = await prisma.cashflow.aggregate({
-            where: {
-                businessId: businessId as string,
-                type: "IN"
-            },
-            _sum: {
-                amount: true
-            }
-        })
-        const totalOutCash = await prisma.cashflow.aggregate({
-            where: {
-                businessId: businessId as string,
-                type: "OUT",
-            },
-            _sum: {
-                amount: true,
-            },
-        });
+
+        const [totalCash, totalOutCash] = await Promise.all([
+            prisma.cashflow.aggregate({
+                where: {
+                    businessId: businessId as string,
+                    type: "IN"
+                },
+                _sum: {
+                    amount: true
+                }
+            }),
+            prisma.cashflow.aggregate({
+                where: {
+                    businessId: businessId as string,
+                    type: "OUT",
+                },
+                _sum: {
+                    amount: true,
+                },
+            })
+
+        ])
 
         const totalInValue = totalCash._sum.amount || 0;
         const totalOutValue = totalOutCash._sum.amount || 0;
+
         return res.json({
             totalIn: totalInValue,
             totalOut: totalOutValue,
