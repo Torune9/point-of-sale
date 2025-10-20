9da9aee70a30702a886293769aed8bc5eb3f813e
rahman <rahmannurhayadi@gmail.com>
2025-10-20 09:47:36 +0700

    feat(component) : add title component

---
+++ pos-fe/src/components/atom/Title.vue
@@ -0,0 +1,24 @@
+<template>
+    <component :is="props.tag" :class="dynamicClass" class="font-medium font-roboto">
+        <slot />
+    </component>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+    tag: 'h1' | 'h2' | 'h3' | 'h4'
+}>();
+
+const dynamicClass = computed<string>(() => {
+    switch (props.tag) {
+        case 'h1':
+            return 'text-3xl lg:text-4xl';
+        case 'h2':
+            return 'text-2xl lg:text-3xl';
+        default:
+            return 'text-xl';
+    }
+});
+</script>
