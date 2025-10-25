8aebb21096b1f5b0a2330dfa4f38f148a851cd4d
rahman <rahmannurhayadi@gmail.com>
2025-10-25 09:08:43 +0700

    feat(compontnent) : add login btn

---
+++ pos-fe/src/components/atom/BurgerBtn.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="w-8 space-y-1 cursor-pointer lg:hidden" @click="showMenu">
+        <span class="block w-full h-1 bg-black"
+            :class="[props.isOpen ? 'rotate-45 translate-y-2' : '', 'transition-all duration-300']" />
+        <span class="block w-full h-1 bg-black" :class="[props.isOpen ? 'scale-0' : '', 'transition-all duration-300']" />
+        <span class="block w-full h-1 bg-black"
+            :class="[props.isOpen ? '-rotate-45 -translate-y-2' : '', 'transition-all duration-300']" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps<{
+    isOpen : boolean
+}>()
+
+const emits = defineEmits < {
+    open: [isOpen : boolean]
+} > ()
+
+const isOpen = ref(false)
+
+const showMenu = () => {
+    isOpen.value = !props.isOpen
+    emits('open', isOpen.value)
+}
+</script>
