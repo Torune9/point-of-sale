53ce4de2563db5d1886b07b7f0b9e857da4d25c4
rahman <rahmannurhayadi@gmail.com>
2025-10-25 09:10:24 +0700

    feat(component) : add custom accordion

--- pos-fe/src/components/organism/CustomSummary.vue
+++ pos-fe/src/components/organism/CustomSummary.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="border-b border-secondary cursor-pointer" @click="toggle">
-        <div class="flex justify-between items-center p-2">
-            <Title tag="h3">
-                {{ title }}
+    <div class="border-b cursor-pointer" v-for="(data, idx) in datas" :key="idx" @click="toggle(idx)">
+        <div class="flex justify-between items-center p-2 transition-colors duration-300"
+            :class="activeIdx == idx ? 'bg-secondary text-white' : ''">
+            <Title :tag="data[keys[1]]">
+                {{ data[keys[0]] }}
             </Title>
             <Transition name="bounce" mode="out-in">
-                <Icon v-if="isOpen" key="minus" :icon="icon" class="w-6 h-6" />
-                <Icon v-else key="plus" :icon="icon" class="w-6 h-6" />
+                <Icon v-if="activeIdx === idx" key="minus" icon="heroicons:minus-solid" class="w-6 h-6" />
+                <Icon v-else key="plus" icon="heroicons:plus-solid" class="w-6 h-6" />
             </Transition>
 
         </div>
-        <Transition name="slide">
-            <div v-if="isOpen">
+
+        <Transition @enter="enter" @leave="leave" appear>
+            <div v-if="idx === activeIdx" :key="idx">
                 <div class="p-2">
-                    <Text>{{ details }}</Text>
+                    <Text>{{ data[keys[2]] }}</Text>
                 </div>
             </div>
         </Transition>
@@ -21,50 +23,74 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue"
-import { Icon } from "@iconify/vue"
+import { computed, ref } from "vue"
 import Title from "../atom/Title.vue";
 import Text from "../atom/Text.vue";
+import { Summary } from "@/types/Summary";
 
 const props = defineProps<{
-    idx: number,
-    title: string,
-    details: string,
-    activeIdx?: number | null
+    datas: Summary[]
 }>()
 
-const emit = defineEmits<{
-    (e: 'toggle', idx: number): void
-}>()
+const listData = computed<Summary[]>(() => props.datas)
 
-const isOpen = computed(() => props.activeIdx === props.idx)
+const keys = computed<string[]>(() => {
+    const first = listData.value?.[0]
+    return first ? Object.keys(first) : []
+})
 
-const icon = computed(() =>
-    isOpen.value ? "heroicons:minus-solid" : "heroicons:plus-solid"
-)
+const activeIdx = ref<number | null>(null)
 
-const toggle = () => {
-    emit('toggle', props.idx)
+const toggle = (idx: number) => {
+    activeIdx.value = activeIdx.value == idx ? null : idx
+}
+
+const enter = (el: Element) => {
+    const node = el as HTMLElement
+    node.style.overflow = 'hidden'
+    node.style.height = '0px'
+    node.style.opacity = '0'
+    const height = node.scrollHeight
+
+    requestAnimationFrame(() => {
+        node.style.transition = 'height 300ms linear, opacity 300ms linear'
+        node.style.height = `${height}px`
+        node.style.opacity = '1'
+    })
+
+    const cleanup = () => {
+        node.style.height = 'auto'
+        node.style.overflow = ''
+        node.style.transition = ''
+        node.removeEventListener('transitionend', cleanup)
+    }
+    node.addEventListener('transitionend', cleanup)
+}
+
+const leave = (el: Element) => {
+    const node = el as HTMLElement
+    node.style.overflow = 'hidden'
+    const height = node.scrollHeight
+    node.style.height = `${height}px`
+    node.style.opacity = '1'
+    void node.offsetHeight
+
+    requestAnimationFrame(() => {
+        node.style.transition = 'height 300ms linear, opacity 300ms linear'
+        node.style.height = '0px'
+        node.style.opacity = '0'
+    })
+
+    const cleanup = () => {
+        node.style.transition = ''
+        node.style.overflow = ''
+        node.removeEventListener('transitionend', cleanup)
+    }
+    node.addEventListener('transitionend', cleanup)
 }
 </script>
 
 <style scoped>
-.slide-enter-active,
-.slide-leave-active {
-    transition: height .5s ease;
-    overflow: hidden;
-}
-
-.slide-enter-from,
-.slide-leave-to {
-    height: 0%;
-}
-
-.slide-enter-to,
-.slide-leave-from {
-    height: max-content;
-}
-
 .bounce-enter-active {
     animation: bounce-in 0.5s;
 }
@@ -77,8 +103,8 @@ const toggle = () => {
     0% {
         transform: scale(0);
     }
-    
-    50%{
+
+    50% {
         transform: scale(1.2);
     }
 
