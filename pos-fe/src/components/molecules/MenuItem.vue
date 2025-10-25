9fe4f098d0421ecb5ba3c9c5484912a55e3d8ab1
rahman <rahmannurhayadi@gmail.com>
2025-10-25 09:10:39 +0700

    feat(component) : add menu item

---
+++ pos-fe/src/components/molecules/MenuItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <Item v-for="(item,idx) in items" :key="idx" v-bind="$attrs">
+        <RouterLink :to="`#${item.path}`" class="lg:text-lg font-medium w-full block py-2">
+            {{ item.name }}
+        </RouterLink>
+    </Item>
+</template>
+
+<script setup lang="ts">
+import { Menu } from '@/types/Menu';
+import Item from '../atom/Item.vue';
+
+const props = defineProps<{
+    items : Array<Menu>
+}>()
+</script>
