089e10934711f38dee6d77a889c5ba61c0b66ed0
rahman <rahmannurhayadi@gmail.com>
2025-10-25 09:09:02 +0700

    feat(compontnent) : add base text component

--- pos-fe/src/components/atom/Text.vue
+++ pos-fe/src/components/atom/Text.vue
@@ -1,5 +1,26 @@
 <template>
-    <p class="text-md">
-        <slot/>
+    <p class="md:text-lg" :class="sizeFontClass">
+        <slot />
     </p>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps < {
+    fontSize?: 'md' | 'lg' | 'xl'
+} > ()
+
+const sizeFontClass = computed(() => {
+    switch (props.fontSize) {
+        case 'md':
+            return 'text-md';
+        case 'lg':
+            return 'text-lg';
+        case 'xl':
+            return 'text-xl';
+        default:
+            break
+    }
+})
+</script>
