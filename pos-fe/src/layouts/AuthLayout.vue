1c6548cb205587c8b5d3f6f50c66c4e9031157c1
rahman <rahmannurhayadi@gmail.com>
2025-11-03 08:44:00 +0700

    feat(layout)) : add base auth layout

--- pos-fe/src/layouts/AuthLayout.vue
+++ pos-fe/src/layouts/AuthLayout.vue
@@ -1,5 +1,10 @@
 <template>
     <div>
-
+        <div class="h-16 fixed p-4 border-b border-b-black/20 w-full">
+            <div class="w-36 left-4 top-4">
+                <img src="../assets/images/logo.png" alt="logo aplikasi" class="invert">
+            </div>
+        </div>
+        <slot />
     </div>
 </template>
