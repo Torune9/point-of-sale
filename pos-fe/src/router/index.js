9cefccb6e5a343975a8796dfb1230f37194c8040
rahman <rahmannurhayadi@gmail.com>
2025-11-03 08:48:51 +0700

    refactor(router) : spread auht router

--- pos-fe/src/router/index.js
+++ pos-fe/src/router/index.js
@@ -1,8 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import { authRouter } from "./authRouter";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name : 'home',
+            component: HomeView,
+            meta : {
+                layout : 'minimal'
+            }
+        },
+        ...authRouter
 
-export default router
+    ],
+});
+
+export default router;
