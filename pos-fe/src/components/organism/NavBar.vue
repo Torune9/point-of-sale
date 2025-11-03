41b36765b4918ed0b3de4f0be8abfb7f752e3d2a
rahman <rahmannurhayadi@gmail.com>
2025-11-03 08:47:50 +0700

    feat(component) : add navbar

---
+++ pos-fe/src/components/organism/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="fixed h-18 w-full flex flex-row justify-between items-center px-4 bg-white z-50 shadow-md">
+        <div>
+            <div class="w-36">
+                <img :src="logo" alt="navbar logo image" class="w-full h-full object-cover invert">
+            </div>
+        </div>
+        <LandingNavbar/>
+    </nav>
+</template>
+
+<script setup>
+import logo from "@/assets/images/logo.png"
+import { ref } from "vue"
+import LandingNavbar from "@/components/molecules/LandingNavbar.vue"
+</script>
