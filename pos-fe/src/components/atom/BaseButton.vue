71b23a064b86f99bb09e38a4c069674b16f38b94
rahman <rahmannurhayadi@gmail.com>
2025-11-03 08:41:44 +0700

    feat(component) : add baseBtn

---
+++ pos-fe/src/components/atom/BaseButton.vue
@@ -0,0 +1,52 @@
+<template>
+    <button :type="type" class="p-2 rounded-full cursor-pointer font-semibold text-white transition-all duration-300"
+        :class="[sizeClass, typeClass]">
+        <slot name="title-btn" />
+    </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+
+const props = withDefaults(defineProps<{
+    size?: 'sm' | 'md' | 'lg' | 'full' | 'auto',
+    typeBtn?: 'danger' | 'info' | 'success' | 'cta',
+    type?: 'submit' | 'button'
+}>()
+    , {
+        type: 'button'
+    })
+const sizeClass = computed<string>(() => {
+    switch (props.size) {
+        case 'sm':
+            return 'w-32';
+        case 'md':
+            return 'w-64';
+        case 'lg':
+            return 'w-96';
+        case 'full':
+            return 'w-full';
+        case 'auto':
+            return 'w-auto'
+        default:
+            return 'w-40'
+    }
+})
+
+const typeClass = computed<string>(() => {
+    switch (props.typeBtn) {
+        case 'danger':
+            return 'bg-red-700 hover:bg-red-600';
+        case 'info':
+            return 'bg-blue-700 hover:bg-blue-600';
+        case 'success':
+            return 'bg-green-700 hover:bg-green-600';
+        case 'cta':
+            return 'bg-secondary hover:bg-secondary/80';
+        default:
+            return 'bg-primary hover:bg-primary/80'
+    }
+})
+
+</script>
--- pos-fe/src/components/atom/LoginBtn.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <button type="button" class="min-h-10 max-h-12 h-full min-w-16 w-full bg-primary rounded-full font-semibold text-white cursor-pointer hover:bg-accent hover:outline hover:text-black transition-all duration-300">
-        Login
-    </button>
-</template>
