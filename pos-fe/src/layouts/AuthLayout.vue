afe3d199f407ffb71b4d675590c6ea957e1dcccf
rahman <rahmannurhayadi@gmail.com>
2025-11-05 10:42:20 +0700

    fix(layout) : add base layout in auth layout

--- pos-fe/src/layouts/AuthLayout.vue
+++ pos-fe/src/layouts/AuthLayout.vue
@@ -1,10 +1,17 @@
+<script setup lang="ts">
+import BaseLayout from './BaseLayout.vue';
+
+</script>
+
 <template>
-    <div>
-        <div class="h-16 fixed p-4 border-b border-b-black/20 w-full">
-            <div class="w-36 left-4 top-4">
-                <img src="../assets/images/logo.png" alt="logo aplikasi" class="invert">
+    <BaseLayout>
+        <template #header>
+            <div class="h-16 fixed p-4 border-b border-b-black/20 w-full">
+                <div class="w-36 left-4 top-4">
+                    <img src="../assets/images/logo.png" alt="logo aplikasi" class="invert">
+                </div>
             </div>
-        </div>
-        <slot />
-    </div>
+            <slot />
+        </template>
+    </BaseLayout>
 </template>
