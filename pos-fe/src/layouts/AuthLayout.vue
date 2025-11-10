47a90137d58a9d5adcee5bd35f318f57ef7e840a
rahman <rahmannurhayadi@gmail.com>
2025-11-10 09:31:37 +0700

    fix(authLayout) : delete baselayout

--- pos-fe/src/layouts/AuthLayout.vue
+++ pos-fe/src/layouts/AuthLayout.vue
@@ -1,17 +1,10 @@
-<script setup lang="ts">
-import BaseLayout from './BaseLayout.vue';
-
-</script>
-
 <template>
-    <BaseLayout>
-        <template #header>
-            <div class="h-16 fixed p-4 border-b border-b-black/20 w-full">
-                <div class="w-36 left-4 top-4">
-                    <img src="../assets/images/logo.png" alt="logo aplikasi" class="invert">
-                </div>
+    <main>
+        <div class="h-16 fixed p-4 border-b border-b-black/20 w-full">
+            <div class="w-36 left-4 top-4">
+                <img src="../assets/images/logo.png" alt="logo aplikasi" class="invert">
             </div>
-            <slot />
-        </template>
-    </BaseLayout>
+        </div>
+        <slot />
+    </main>
 </template>
