0c2dd31865d154af5813962f76e6492cd0546273
rahman <rahmannurhayadi@gmail.com>
2025-11-10 09:31:17 +0700

    style(layout) : fix hierachy

--- pos-fe/src/layouts/MinimalLayout.vue
+++ pos-fe/src/layouts/MinimalLayout.vue
@@ -1,17 +1,12 @@
 <template>
-    <BaseLayout>
-        <template #header>
-            <NavBar/>
-        </template>
-        <template #content>
-            <div class="box-border flex flex-col gap-y-10">
-                <slot/>
-            </div>
-        </template>
-    </BaseLayout>
+    <header class="sticky top-0">
+        <NavBar/>
+    </header>
+    <main class="wrapper">
+        <slot/>
+    </main>
 </template>
 
 <script setup lang="ts">
 import NavBar from '@/components/organism/NavBar.vue';
-import BaseLayout from './BaseLayout.vue';
 </script>
