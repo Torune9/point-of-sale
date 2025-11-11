08b6ad87d057aae2f59a35aea4d8ee2152d4cd54
rahman <rahmannurhayadi@gmail.com>
2025-11-11 08:48:45 +0700

    feat(router) : add notfoundview

--- pos-fe/src/router/index.js
+++ pos-fe/src/router/index.js
@@ -2,21 +2,29 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import { authRouter } from "./authRouter";
 import { mainRouter } from "./mainRouter";
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: '/',
-            name : 'home',
+            path: "/",
+            name: "home",
             component: HomeView,
-            meta : {
-                layout : 'minimal'
-            }
+            meta: {
+                layout: "minimal",
+            },
         },
         ...authRouter,
-        ...mainRouter
-
+        ...mainRouter,
+        {
+            path: "/:pathMatch(.*)*",
+            name: "notfound",
+            component: NotFoundView,
+            meta : {
+                layout : "empty"
+            }
+        },
     ],
 });
 
