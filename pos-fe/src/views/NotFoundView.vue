5b39ee62164043ae11339e269e5564fe49824248
rahman <rahmannurhayadi@gmail.com>
2025-11-11 08:48:31 +0700

    feat(view) : add notfoundview

---
+++ pos-fe/src/views/NotFoundView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex flex-col gap-y-2 justify-center items-center h-screen font-roboto">
+        <h1 class="text-8xl font-black">404</h1>
+        <p class="max-w-[30ch] text-center">Sorry,the resource requested could be not found.</p>
+    </div>
+</template>
