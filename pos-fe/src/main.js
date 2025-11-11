630c2c0679a1f7382a06a79f8d7ef204e1114600
rahman <rahmannurhayadi@gmail.com>
2025-11-11 08:28:02 +0700

    use vue toastify & pinia

--- pos-fe/src/main.js
+++ pos-fe/src/main.js
@@ -1,17 +1,25 @@
 import "../src/css/style.css";
 
 import { createApp } from "vue";
-import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
 
 import { Icon } from "@iconify/vue";
 
+import Vue3Toastify from "vue3-toastify";
+import 'vue3-toastify/dist/index.css';
+import { pinia } from "./helper/pinia";
+
 const app = createApp(App);
 
-app.component('Icon',Icon)
-app.use(createPinia());
+app.use(Vue3Toastify, {
+    autoClose: 3000,
+});
+
+app.component("Icon", Icon);
+
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
