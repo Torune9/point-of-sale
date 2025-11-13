5cc16472214ce3b8609e585e046bff1917ea1ce7
rahman <rahmannurhayadi@gmail.com>
2025-11-13 09:05:35 +0700

    fix import file router

--- pos-fe/src/router/index.js
+++ pos-fe/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
-import { authRouter } from "./authRouter";
-import { mainRouter } from "./mainRouter";
+import { authRouter } from "./authRouter.js";
+import { mainRouter } from "./mainRouter.js";
 import NotFoundView from "@/views/NotFoundView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(import.meta.env.VITE_BASE_URL),
     routes: [
         {
             path: "/",
@@ -21,8 +21,8 @@ const router = createRouter({
             path: "/:pathMatch(.*)*",
             name: "notfound",
             component: NotFoundView,
-            meta : {
-                layout : "empty"
+            meta: {
+                layout: "empty"
             }
         },
     ],
