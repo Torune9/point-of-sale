8270ba62cb0e62764827b3a0281e6aee6fa096fb
rahman <rahmannurhayadi@gmail.com>
2025-11-13 09:05:11 +0700

    set global vue easy table component

--- pos-fe/src/main.js
+++ pos-fe/src/main.js
@@ -3,13 +3,16 @@ import "../src/css/style.css";
 import { createApp } from "vue";
 
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/index.js";
 
 import { Icon } from "@iconify/vue";
+import { pinia } from "./helper/pinia.js";
 
 import Vue3Toastify from "vue3-toastify";
 import 'vue3-toastify/dist/index.css';
-import { pinia } from "./helper/pinia";
+
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
 
 const app = createApp(App);
 
@@ -17,6 +20,7 @@ app.use(Vue3Toastify, {
     autoClose: 3000,
 });
 
+app.component('EasyTable', Vue3EasyDataTable);
 app.component("Icon", Icon);
 
 app.use(pinia);
