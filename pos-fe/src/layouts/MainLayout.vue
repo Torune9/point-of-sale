264ff9cfe4577cd50d048a2b2b3fe599fc052fc9
rahman <rahmannurhayadi@gmail.com>
2025-11-13 09:25:54 +0700

    style(layout) : fix z index

--- pos-fe/src/layouts/MainLayout.vue
+++ pos-fe/src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="min-h-screen grid grid-rows-[auto_1fr] lg:grid-cols-[auto_1fr]">
 
-        <header class="p-2 lg:col-start-2 sticky top-0">
+        <header class="p-2 lg:col-start-2 sticky top-0 z-20">
             <div class="flex justify-between items-center p-2 bg-gray-100 rounded-2xl">
                 <div class="w-32">
                     <img src="../assets/images/logo.png" alt="image logo" class="invert">
@@ -22,7 +22,7 @@
             </div>
         </header>
 
-        <aside class="scroll-hidden z-1 h-screen lg:overflow-hidden w-64 fixed bg-gray-100 transition-all duration-300 lg:sticky lg:top-0 lg:translate-x-0 lg:row-start-1 lg:row-span-2 box-border" :class="isShow ? 'translate-x-0' : '-translate-x-full'">
+        <aside class="scroll-hidden z-50 h-screen lg:overflow-hidden w-64 fixed bg-gray-100 transition-all duration-300 lg:sticky lg:top-0 lg:translate-x-0 lg:row-start-1 lg:row-span-2 box-border" :class="isShow ? 'translate-x-0' : '-translate-x-full'">
             <button class="absolute top-1/2 left-full bg-primary text-white rounded-tr rounded-br p-1 lg:hidden" @click="showSideBar">
                 <Icon icon="heroicons:arrow-right-20-solid" class="transition-all duration-500" :class="isShow ? 'rotate-y-180' : 'rotate-y-0'"/>
             </button>
@@ -35,7 +35,7 @@
                 <MultiMenu :menu="listMenu"/>
             </div>
         </aside>
-        <Overlay v-if="isShow" @click="showSideBar" class="lg:hidden"/>
+        <Overlay v-if="isShow" @click="showSideBar" class="lg:hidden z-30"/>
 
         <main class="lg:col-start-2 lg:row-span-2 p-2 ">
             <slot />
