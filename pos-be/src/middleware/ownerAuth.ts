73d817ef49aa5e4bfeb08a93a0c1f416e2a1786f
rahman <rahmannurhayadi@gmail.com>
2025-11-16 10:15:08 +0700

    feat(middleware) : add onwerAuth

---
+++ pos-be/src/middleware/ownerAuth.ts
@@ -0,0 +1,23 @@
+import type { NextFunction, Request, Response } from "express"
+import { handleJwtError } from "../utils/jwtError.js"
+import { roleCheck } from "../helper/roleCheck.js"
+
+export const ownerAuth = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const [scheme, token] = (req.get("Authorization") ?? "").split(" ")
+
+        const result = await roleCheck(token as string, "owner")
+
+        if (!result.ok) {
+            return res.status(result.code as number).json({
+                message : result.message,
+                code : result.code
+            })
+        }
+
+        next()
+
+    } catch (error) {
+        return handleJwtError(error, res)
+    }
+}
