790749c3af4c458bb8225d562904e78e3cece05d
rahman <rahmannurhayadi@gmail.com>
2025-11-20 09:28:03 +0700

    feat(router) : add finance & invent routes

--- pos-fe/src/router/financeRouter.ts
+++ pos-fe/src/router/financeRouter.ts
@@ -1,16 +1,25 @@
 import { ChildRouteType } from "@/types/routes";
 import CashFlowView from "@/views/main/finance/CashFlowView.vue";
+import SalesView from "@/views/main/finance/SalesView.vue";
 
 export const financeRouter: ChildRouteType[] = [
     {
         path: '/finance',
         name: 'finance',
+        redirect : {
+            name : 'cashFlow'
+        },
         children: [
             {
                 path: 'cash-flow',
                 name: 'cashFlow',
                 component: CashFlowView,
-            }
+            },
+            {
+                path: 'sales',
+                name: 'sales',
+                component: SalesView,
+            },
         ],
         meta: {
             layout: "main"
--- pos-fe/src/router/inventoryRouter.ts
+++ pos-fe/src/router/inventoryRouter.ts
@@ -1,16 +1,23 @@
 import { ChildRouteType } from "@/types/routes";
 import ProductView from "@/views/main/inventory/ProductView.vue";
+import StockMovementView from "@/views/main/inventory/StockMovementView.vue";
 
 export const inventoryRouter: ChildRouteType[] = [
     {
         name: 'inventory',
         path: '/inventory',
+        redirect : {name : 'product'},
         children: [
             {
                 name: 'product',
                 path: 'products',
                 component : ProductView
-            }
+            },
+            {
+                name: 'stockMovent',
+                path: 'stock-movements',
+                component : StockMovementView
+            },
         ],
          meta: {
             layout: 'main'
