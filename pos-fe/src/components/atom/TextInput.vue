0cf35db624c1ac4a5368bc3a7b490c7fde165d29
rahman <rahmannurhayadi@gmail.com>
2025-11-24 10:30:23 +0700

    fix(component) : fix disable attribute in textInput

--- pos-fe/src/components/atom/TextInput.vue
+++ pos-fe/src/components/atom/TextInput.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="flex flex-col gap-y-1 w-full">
-        <label :for="label" class="font-medium capitalize">
+    <div class="flex flex-col w-full">
+        <label :for="label" class="font-medium capitalize max-sm:text-sm mb-2">
             {{ label }}
         </label>
-        <input :type="type" :name="label" :id="label" class="w-full border border-black/50 focus:outline-primary p-2 rounded-lg hover:outline-1 hover:outline-accent transition-all duration-500" v-model="model" :placeholder="placeholder">
+        <input :type="type" :name="label" :id="label" class="w-full border border-black/20 focus:outline-primary p-2 rounded-lg hover:outline-1 hover:outline-accent transition-all duration-500" v-model="model" :placeholder="placeholder" :disabled="disabled" :class="{
+            'bg-gray-300' : disabled
+        }">
         <small class="text-red-700">
             {{ errorMessage ? errorMessage[0].$message : '' }}
         </small>
@@ -17,9 +19,11 @@ const props = withDefaults(defineProps<{
     label?: string,
     type?: 'text' | 'password' | 'number',
     errorMessage?: Array<ErrorObject> | null,
-    placeholder? :string
+    placeholder? :string,
+    disabled? : boolean
 }>(), {
-    type: 'text'
+    type: 'text',
+    disabled : false
 })
 
 const model = defineModel<string | number>({ required: true })
