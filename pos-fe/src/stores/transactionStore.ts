9d42857187a83804f635aaf6cc9a8fe6f2fbbb50
rahman <rahmannurhayadi@gmail.com>
2025-12-03 09:26:29 +0700

    feat(store) : add transacrion store

---
+++ pos-fe/src/stores/transactionStore.ts
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+import { CheckoutItems } from "@/types/checkoutItem";
+import { notify } from "@/helper/toastifyHelper";
+
+export const useTransactionStore = defineStore('transaction', {
+    state: () => ({
+        items: []
+    }),
+    persist: true,
+    actions: {
+        pushToItem(item: CheckoutItems) {
+            const idx = this.items.findIndex((val) => item.id == val.id)
+            if (idx !== -1) {
+                this.items[idx].quantity++
+                this.items[idx].totalPrice  = this.items[idx].price * this.items[idx].quantity
+                notify.success('quantity has been updated')
+                return
+            }
+            this.items.push(item)
+            notify.success('item has been added')
+        },
+        deleteItem(id: number) {
+            this.items = this.items.filter((item: CheckoutItems) => item.id !== id)
+            notify.warning('item has been deleted')
+        },
+        updateItem(selectedItem?: CheckoutItems, quantity?: number, totalPrice?: number) {
+            const idx = this.items.findIndex((val) => selectedItem.id == val.id)
+            if (idx !== -1) {
+                this.items[idx].quantity += quantity
+                this.items[idx].totalPrice += totalPrice
+                notify.success('quantity has been updated')
+            }
+        },
+        quantityIncrement(idx: number) {
+            this.items[idx - 1].quantity++
+            this.items[idx - 1].totalPrice += this.items[idx - 1].price
+        },
+        quantityDecrement(idx: number) {
+            this.items[idx - 1].quantity--
+            this.items[idx - 1].totalPrice -= this.items[idx - 1].price
+        }
+    }
+})
