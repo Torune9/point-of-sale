4c1406abd28a329a3a249cce59a62f3bf78bd339
rahman <rahmannurhayadi@gmail.com>
2025-12-04 10:43:40 +0700

    fix(controller) : fix qrcode when update data

--- pos-be/src/controller/inventories/products/update.ts
+++ pos-be/src/controller/inventories/products/update.ts
@@ -29,7 +29,7 @@ export const updateProduct = async (req: Request, res: Response, next: NextFunct
         const qrData = JSON.stringify({
             id: existingProduct.id,
             sku: existingProduct.sku,
-            name: existingProduct.name,
+            name: name ?? existingProduct.name,
             price: existingProduct.price
         })
         const qrUrl = await QrCode.toDataURL(qrData)
@@ -51,8 +51,6 @@ export const updateProduct = async (req: Request, res: Response, next: NextFunct
                         : existingProduct.price,
             },
         });
-
-
 
         return res.json({
             message: "Product has been updated",
