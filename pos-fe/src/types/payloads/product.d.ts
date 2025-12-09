409133881bb6e6f4916b9808737f981bba2ae328
rahman <rahmannurhayadi@gmail.com>
2025-12-09 08:13:32 +0700

    fix(type) : add update item type

--- pos-fe/src/types/payloads/product.d.ts
+++ pos-fe/src/types/payloads/product.d.ts
@@ -1,6 +1,30 @@
-export interface Product {
-    name : string,
-    category : string,
-    price : string,
-    stock : string,
+export interface Category {
+    name: string,
+    id: string,
+    businessId: string,
 }
+
+export interface Product {
+    name: string,
+    category: Category,
+    price: string,
+    stock: number,
+    barcode?: string
+}
+
+export interface ProductUpload {
+    name: string,
+    categoryId: string,
+    price: number,
+    stock: number,
+    businessId: string
+}
+
+
+export type UpdateProduct = {
+    name: string,
+    price: number,
+    stock: number,
+    categoryId: string
+}
+export type UpdateItems = Product & Category 
