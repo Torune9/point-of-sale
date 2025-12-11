dc9d01ef9f3408ba0ffd87709244c4119622c656
rahman <rahmannurhayadi@gmail.com>
2025-12-11 10:46:53 +0700

    feat(controller) : add get cashflow func in cashflow

--- pos-be/src/controller/finance/cashFlow.ts
+++ pos-be/src/controller/finance/cashFlow.ts
@@ -1,28 +1,51 @@
-import type { Request,Response } from "express";
+import type { Request, Response } from "express";
 import prisma from "../../utils/prisma.js";
 
 export const cashFlow = async (req: Request, res: Response) => {
     try {
-        const { type, amount, businessId, workerId,note } = req.body
+        const { type, amount, businessId, workerId, note } = req.body
         const cash = await prisma.cashflow.create({
-            data : {
+            data: {
                 type,
                 amount,
                 businessId,
-                workerId : workerId || null,
+                workerId: workerId || null,
                 note
             }
         })
 
         return res.status(201).json({
-            message : 'cash flow has been created',
+            message: 'cash flow has been created',
             code: res.statusCode,
-            data : cash
+            data: cash
         })
     } catch (error) {
         return res.status(500).json({
-            message : 'error  on server when recap cashflow',
-            code : res.statusCode,
+            message: 'error  on server when recap cashflow',
+            code: res.statusCode,
+            errors: error
+        })
+    }
+}
+
+export const getCashFlow = async (req: Request, res: Response) => {
+    try {
+        const { businessId } = req.params
+        const cash = await prisma.cashflow.findMany({
+            where: {
+                businessId: businessId as string
+            }
+        })
+
+        return res.status(201).json({
+            message: 'cash flow has been created',
+            code: res.statusCode,
+            data: cash
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'error  on server when recap cashflow',
+            code: res.statusCode,
             errors: error
         })
     }
