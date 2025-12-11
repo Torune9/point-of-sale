ffe90595ee91a3dec0ab14f54f31185fe4c62021
rahman <rahmannurhayadi@gmail.com>
2025-12-11 10:47:10 +0700

    feat(router) : add get cashflow in router

--- pos-be/src/router/routes/cashFlow.ts
+++ pos-be/src/router/routes/cashFlow.ts
@@ -1,11 +1,11 @@
 import e from "express";
 import { totalCash } from "../../controller/finance/totalCash.js";
-import { cashFlow } from "../../controller/finance/cashFlow.js";
+import { cashFlow, getCashFlow } from "../../controller/finance/cashFlow.js";
 import { validateData } from "../../middleware/validationMiddleware.js";
 import { cashData } from "../../schemas/cashFlowSchema.js";
 
 export const cashRouter = e.Router()
 
 cashRouter.post('/',validateData(cashData),cashFlow)
-
-cashRouter.get('/:id',totalCash)
+cashRouter.get('/:businessId',getCashFlow)
+cashRouter.get('/:businessId/:id',totalCash)
