146fd677acd209ba251409de739c12a3da1a5560
rahman <rahmannurhayadi@gmail.com>
2025-12-11 09:44:50 +0700

    fix(type) : ad role prop in responseLogin type

--- pos-fe/src/types/response.d.ts
+++ pos-fe/src/types/response.d.ts
@@ -1,13 +1,14 @@
 export interface ResponseLogin {
     message: string,
-    business : object[]
+    business: object[]
     token: string,
     code: number,
-    data: object[]
+    data: object[],
+    role: object
 }
 
 export interface ResponseData {
-    message : string,
-    code : number,
-    data : any,
+    message: string,
+    code: number,
+    data: any,
 }
