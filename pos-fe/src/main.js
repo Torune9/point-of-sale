5d981e6866c322c3323894e1dd726c5f0baa86ce
rahman <rahmannurhayadi@gmail.com>
2025-12-22 09:59:42 +0700

    fix spaciing

--- pos-fe/src/main.js
+++ pos-fe/src/main.js
@@ -9,10 +9,10 @@ import { Icon } from "@iconify/vue";
 import { pinia } from "./helper/pinia.js";
 
 import Vue3Toastify from "vue3-toastify";
-import 'vue3-toastify/dist/index.css';
+import "vue3-toastify/dist/index.css";
 
-import Vue3EasyDataTable from 'vue3-easy-data-table';
-import 'vue3-easy-data-table/dist/style.css';
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
 
 const app = createApp(App);
 
@@ -20,7 +20,7 @@ app.use(Vue3Toastify, {
     autoClose: 3000,
 });
 
-app.component('EasyTable', Vue3EasyDataTable);
+app.component("EasyTable", Vue3EasyDataTable);
 app.component("Icon", Icon);
 
 app.use(pinia);
