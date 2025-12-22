06041551e712b58a0dfd0e5c26f04421c600976e
rahman <rahmannurhayadi@gmail.com>
2025-12-22 09:59:59 +0700

    feat(component) : add vue datepicker

---
+++ pos-fe/src/components/atom/VueDatePicker.vue
@@ -0,0 +1,11 @@
+<template>
+    <VueDatePicker v-model="date" placeholder="Select Date"></VueDatePicker>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { VueDatePicker } from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const date = ref();
+</script>
