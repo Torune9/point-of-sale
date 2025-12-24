b52730435df8c5ed48ca34ec27449da416df38c0
rahman <rahmannurhayadi@gmail.com>
2025-12-24 08:31:57 +0700

    feat : add get stock movement ccontroller

---
+++ pos-be/src/controller/inventories/stock-movements/getStockMovements.ts
@@ -0,0 +1,24 @@
+import type { Request, Response } from "express";
+import prisma from "../../../utils/prisma.js";
+
+export const getStockMovements = async (req: Request, res: Response) => {
+    try {
+        const { businessId } = req.params
+        const stockMovements = await prisma.stockMovement.findMany({
+            where: {
+                businessId: businessId as string
+            }
+        })
+        return res.json({
+            message: "stock movement has been retrieved",
+            data: stockMovements,
+            code: res.statusCode
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'error on server when get stock movements',
+            error,
+            code: res.statusCode
+        })
+    }
+}
--- pos-be/src/router/routes/stockMovementRoutes.ts
+++ pos-be/src/router/routes/stockMovementRoutes.ts
@@ -3,8 +3,11 @@ import { stockMovement } from "../../controller/inventories/stock-movements/stoc
 import { validateData } from "../../middleware/validationMiddleware.js"
 import { CreateSaleSchema, ProductMovement } from "../../schemas/stockMovementSchema.js"
 import { stockOutSelling } from "../../controller/inventories/stock-movements/stockOut.js"
+import { getStockMovements } from "../../controller/inventories/stock-movements/getStockMovements.js"
 
 export const stockMomeventRouter = e.Router()
+
+stockMomeventRouter.get('/', getStockMovements)
 
 stockMomeventRouter.post('/', validateData(ProductMovement), stockMovement)
 
