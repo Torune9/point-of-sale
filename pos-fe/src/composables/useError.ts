e59bc0ec5a49a569ffe1180e520d5d6de1a30217
rahman <rahmannurhayadi@gmail.com>
2025-12-30 08:47:57 +0700

    fix use error

--- pos-fe/src/composables/useError.ts
+++ pos-fe/src/composables/useError.ts
@@ -4,13 +4,15 @@ import { AxiosError } from "axios"
 
 export const useError = () => {
     const networkErr = (err: AxiosError) => {
-        if (err.code) {
-            notify.error(err.message)
-        }
+        notify.error(err.message)
     }
     const serverError = (err: AxiosError) => {
-        const error = err.response.data as ResponseData
-        notify.error(error.message)
+        const error: ResponseData = err.response.data as ResponseData
+        if (error?.error) {
+            notify.error(error?.error)
+        } else {
+            notify.error(error.message)
+        }
     }
 
     return {
