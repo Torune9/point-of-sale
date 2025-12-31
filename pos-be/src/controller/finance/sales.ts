2afecb7e666f69b13553436f915bdb0cfd8c5de7
rahman <rahmannurhayadi@gmail.com>
2025-12-31 08:23:55 +0700

    feat(sales) : add get annual sales

--- pos-be/src/controller/finance/sales.ts
+++ pos-be/src/controller/finance/sales.ts
@@ -37,3 +37,39 @@ export const getSales = async (req: Request, res: Response, next: NextFunction) 
     }
 };
 
+export const getAnnualSales = async (req: Request, res: Response) => {
+    const { businessId } = req.params
+
+    try {
+        const result = await prisma.$queryRaw<
+            { month: Date; total: number }[]
+        >`
+        WITH months AS (
+        SELECT generate_series(
+            date_trunc('year', now()),
+            date_trunc('year', now()) + interval '11 month',
+            interval '1 month'
+        ) AS month
+        )
+        SELECT
+        m.month,
+        COALESCE(SUM(s."totalAmount"), 0) AS total
+        FROM months m
+        LEFT JOIN sales s
+        ON date_trunc('month', s."createdAt") = m.month
+        AND s."businessId" = ${businessId}
+        GROUP BY m.month
+        ORDER BY m.month;
+    `
+
+        return res.json({
+            message: 'data has been retrieved',
+            data: result
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'error on server when getting data sales',
+            error
+        })
+    }
+}
--- pos-be/src/router/routes/salesRouter.ts
+++ pos-be/src/router/routes/salesRouter.ts
@@ -1,8 +1,9 @@
 import e from "express";
 import { salesReport } from "../../controller/finance/salesReport.js";
-import { getSales } from "../../controller/finance/sales.js";
+import { getAnnualSales, getSales } from "../../controller/finance/sales.js";
 
 export const salesRouter = e.Router()
 
 salesRouter.get('/:businessId', getSales)
+salesRouter.get('/annual/:businessId',getAnnualSales)
 salesRouter.get('/report/:businessId', salesReport)
