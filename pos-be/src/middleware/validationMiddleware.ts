977bd655fa41c878d13ebf1b17c20e0258801706
rahman <rahmannurhayadi@gmail.com>
2026-01-01 10:57:11 +0700

    fix(middleware) : fix body parse data

--- pos-be/src/middleware/validationMiddleware.ts
+++ pos-be/src/middleware/validationMiddleware.ts
@@ -1,20 +1,24 @@
-import type { Request, Response, NextFunction } from "express";
-import { z, ZodError } from "zod";
+import type { Request, Response, NextFunction } from "express"
+import { z, ZodError } from "zod"
 
-export const validateData = (schema: z.ZodObject<any, any>) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        try {
-            schema.parse(req.body)
-            next()
-        } catch (error) {
-            if (error instanceof ZodError) {
-                const errorMessages = error.issues.map((issue: any) => ({
-                    message: `${issue.path.join('.')} is ${issue.message}`,
-                }))
-                res.status(400).json({ error: 'Invalid data', details: errorMessages });
-            } else {
-                res.status(500).json({ error: 'Internal Server Error' });
+export const validateData =
+    (schema: z.ZodSchema) =>
+        (req: Request, res: Response, next: NextFunction) => {
+            try {
+                const parsedData = schema.parse(req.body)
+                req.body = parsedData
+                next()
+            } catch (error) {
+                if (error instanceof ZodError) {
+                    return res.status(400).json({
+                        message: "Validation error",
+                        errors: error.issues.map(issue => ({
+                            field: issue.path.join("."),
+                            message: issue.message
+                        }))
+                    })
+                }
+
+                return res.status(500).json({ message: "Internal Server Error" })
             }
         }
-    }
-}
