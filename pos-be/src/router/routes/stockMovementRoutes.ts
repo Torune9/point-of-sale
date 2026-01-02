6251459c773c4d1263d62c4c94b182ffac17625e
rahman <rahmannurhayadi@gmail.com>
2026-01-02 08:29:36 +0700

    fix(sockRoutes) : add middleware

--- pos-be/src/router/routes/stockMovementRoutes.ts
+++ pos-be/src/router/routes/stockMovementRoutes.ts
@@ -4,11 +4,13 @@ import { validateData } from "../../middleware/validationMiddleware.js"
 import { CreateSaleSchema, ProductMovement } from "../../schemas/stockMovementSchema.js"
 import { stockOutSelling } from "../../controller/inventories/stock-movements/stockOut.js"
 import { getStockMovements } from "../../controller/inventories/stock-movements/getStockMovements.js"
+import { adminAuth } from "../../middleware/adminAuthorization.js"
+import { cashierAuth } from "../../middleware/cashierAuth.js"
 
 export const stockMomeventRouter = e.Router()
 
 stockMomeventRouter.get('/:businessId', getStockMovements)
 
-stockMomeventRouter.post('/', validateData(ProductMovement), stockMovement)
+stockMomeventRouter.post('/', validateData(ProductMovement),adminAuth, stockMovement)
 
-stockMomeventRouter.post('/selling', validateData(CreateSaleSchema), stockOutSelling)
+stockMomeventRouter.post('/selling', validateData(CreateSaleSchema),cashierAuth, stockOutSelling)
