d0dbf289720f610ba7bc97db116ba1b977ab6616
rahman <rahmannurhayadi@gmail.com>
2026-01-02 09:33:31 +0700

    fix(categryStore) : fix rsponse

--- pos-fe/src/stores/categoryStore.ts
+++ pos-fe/src/stores/categoryStore.ts
@@ -1,3 +1,4 @@
+import { handleError } from "@/helper/errors";
 import { notify } from "@/helper/toastifyHelper";
 import { api } from "@/services/apiService";
 import { ResponseData } from "@/types/response";
@@ -12,11 +13,33 @@ export const categoryStore = defineStore("catgeory", {
             try {
                 const response = await api.get(`/categories/${businessId}`)
                 const result: ResponseData = response.data
-                return result
+                return { ok: true, result }
             } catch (error) {
-                notify.error("error when getting categories")
-                throw error
+                handleError(error)
+                return { ok: false, error }
             }
-        }
+        },
+        async createCategory(payload: { name: string, businessId: string }) {
+            try {
+                const response = await api.post(`/categories`, payload)
+                const result: ResponseData = response.data
+                notify.success(result.message)
+                return { ok: true, result }
+            } catch (error) {
+                handleError(error)
+                return { ok: false, error }
+            }
+        },
+        async deleteCategory(businessId: string, produdctId: string) {
+            try {
+                const response = await api.delete(`/categories/${businessId}/${produdctId}`)
+                const result: ResponseData = response.data
+                notify.success(result.message)
+                return { ok: true, result }
+            } catch (error) {
+                handleError(error)
+                return { ok: false, error }
+            }
+        },
     }
 })
