4fe7d0d4fc26255a466fa618fbdf942cea4533c3
rahman <rahmannurhayadi@gmail.com>
2026-01-07 08:57:09 +0700

    feat : add ini socket

--- pos-be/src/index.ts
+++ pos-be/src/index.ts
@@ -3,12 +3,16 @@ import "dotenv/config"
 import express from 'express'
 import router from './router/router.js'
 import cors from 'cors'
+import { Server } from 'socket.io'
 
 import { errorHandlerPrisma } from './middleware/prismaError.js'
 import { logger } from "./utils/logger.js"
+import { createServer } from "node:http"
+import { initSocket } from "./socket.js"
 
 const port = 3000
 const app = express()
+const server = createServer(app)
 
 app.use(express.json())
 
@@ -21,6 +25,8 @@ app.use('/api', router)
 
 app.use(errorHandlerPrisma)
 
-app.listen(port, () => {
+initSocket(server)
+
+server.listen(port, () => {
     logger.info(`server running on ${port}`)
 })
